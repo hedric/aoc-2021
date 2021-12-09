e971ca557238d0cba34ff54a007099ead2658c81
Richard Hedlund <richard.hedlund@gmail.com>
2021-12-09 22:51:17 +0100

    day1: initial commit solving part1

---
+++ day1/main.cpp
@@ -0,0 +1,46 @@
+/* Advent of code day 1 */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main(){
+
+	//Part1: How quickly does depth increase?
+	
+	// construct file stream 
+	std::ifstream input("input.txt");
+
+	int incr = 0;
+	int prev = 0;
+	int curr = 0;
+
+	std::vector<int> measurements;
+	
+	// loop through the file stream
+	// cast string entires to integers
+	// push integers to vector
+
+	if (input.is_open()){
+		std::string line;
+		while (std::getline(input, line)){
+
+			curr = std::stoi(line);
+			
+			if (curr > prev && prev != 0){
+				incr++;
+			}
+			prev = curr;
+			measurements.push_back(curr);
+
+		}
+	}
+
+	std::cout<<"Total measurements: "<<measurements.size()<<std::endl;
+	std::cout<<"Number of increases: "<<incr<<std::endl;
+
+	input.close();
+
+	return 0;
+}
